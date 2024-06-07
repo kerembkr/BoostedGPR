ad9bee05ad5e960fea331ed8fafa83766e7a33a2
Kerem Bükrü <kerembuekrue@gmail.com>
2024-06-07 17:50:22 +0200

    added new file hyperopt.py with GridSearch and RandomSearch classes

---
+++ src/optim/hyperopt.py
@@ -0,0 +1,17 @@
+
+class HyperOpt:
+
+    def __init__(self):
+        pass
+
+
+class GridSearch(HyperOpt):
+
+    def __init__(self):
+        super().__init__()
+
+
+class RandomSearch(HyperOpt):
+
+    def __init__(self):
+        super().__init__()
