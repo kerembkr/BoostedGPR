4c24b7f8c21f1806f6e1d1f136ac99945d5a4fae
Kerem Bükrü <kerembuekrue@gmail.com>
2024-06-09 13:47:19 +0200

    added save_fig function

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,4 +1,6 @@
+import os
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def data_from_func(f, N, M, xx, noise=0.1):
@@ -24,3 +26,10 @@ def data_from_func(f, N, M, xx, noise=0.1):
     X_test = np.linspace(xmin_te, xmax_te, M).reshape(-1, 1)
 
     return X_train, X_test, y_train
+
+
+def save_fig(name):
+    output_dir = '../../output/'
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    plt.savefig(os.path.join(output_dir, name + ".png"))
